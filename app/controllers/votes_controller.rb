d173ed8258c0d8b7bc8ef4ce52f688eb308aada7
sweetkeys <shaun@shaunsweet.com>
2016-05-13 14:44:46 -0700

    add votes functionality to show page

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,15 +1,21 @@
 #route for adding vote to a question
-post '/users/1/questions/1/vote' do
+get '/users/:user_id/questions/:question_id/vote' do
+	question = Question.find(params[:question_id])
+	question.votes.create
   
+  redirect "/users/#{session[:id].id}/questions/#{question.id}"
 end
 
 #route for adding vote to an answer
-post '/users/:id/answers/:id/vote' do
-  
+get '/users/:user_id/answers/:answer_id/vote' do
+  answer = Answer.find(params[:answer_id])
+  answer.votes.create
+
+  redirect "/users/#{session[:id].id}/questions/#{answer.question.id}"
 end
 
 get '/users/:id/responses/:id/vote' do
   p "hitting post"
- 
+
 end
 
