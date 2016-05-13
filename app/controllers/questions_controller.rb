026830cbb72c2d76cd5ca4820a9b1a65e5ebab48
sweetkeys <shaun@shaunsweet.com>
2016-05-13 10:55:45 -0700

    remove random comments

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,13 +1,13 @@
 get '/users/:user_id/questions' do
 
-  #your code here
+  
 
 end
 
 get '/users/:user_id/questions/new' do
   
   
-  #your code here
+  
   erb :"/questions/new"
 end
 
@@ -15,33 +15,35 @@ post '/users/:user_id/questions' do
 
   @question = Question.create(title: params[:title], body: params[:body], user_id: 1)
   if @question
-  redirect "/users/:user_id/questions/:id"
+
+    #this route needs to be changed 
+  redirect "/users/1/questions/#{Question.last.id}"
   else
+
     #change this redirect to pass the correct session ID
     redirect '/users/1/questions/new'
   end
 end
 
 get '/users/:user_id/questions/:id' do
-  # Individual view of a question
-  #your code here
+
   erb :"/users/question"
 end
 
  get '/users/:user_id/questions/:id/edit' do
 
-  #your code here
+  
 
 end
 
  put '/users/:user_id/questions/:id' do
 
-  #your code here
+  
 
 end
 
 delete '/users/:user_id/questions/:id' do
 
-  #your code here
+  
 
 end
