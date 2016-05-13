14ae202c58c6f0d98a5d957349f1c8b97a1eb4c5
sweetkeys <shaun@shaunsweet.com>
2016-05-12 19:53:59 -0700

    add "working" seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
   has_secure_password
-  
+  has_many :responses
+  has_many :questions
+  has_many :answers
+  has_many :votes
 end
