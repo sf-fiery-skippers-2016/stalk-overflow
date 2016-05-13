ae43405c20562879fb2151b3a96f1a9f752f6d49
sweetkeys <shaun@shaunsweet.com>
2016-05-13 14:13:04 -0700

    add vote links

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,7 +4,12 @@ post '/users/1/questions/1/vote' do
 end
 
 #route for adding vote to an answer
-post '/users/:id/questions/:id/answers/:id/vote' do
+post '/users/:id/answers/:id/vote' do
   
+end
+
+get '/users/:id/responses/:id/vote' do
+  p "hitting post"
+ 
 end
 
