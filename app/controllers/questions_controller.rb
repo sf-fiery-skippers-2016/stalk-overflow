ababc79de57077f48f4df9c7088858dd0fe354df
sweetkeys <shaun@shaunsweet.com>
2016-05-13 10:51:07 -0700

    add question create feature

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,15 +5,21 @@ get '/users/:user_id/questions' do
 end
 
 get '/users/:user_id/questions/new' do
-
+  
+  
   #your code here
   erb :"/questions/new"
 end
 
 post '/users/:user_id/questions' do
 
-  #your code here
+  @question = Question.create(title: params[:title], body: params[:body], user_id: 1)
+  if @question
   redirect "/users/:user_id/questions/:id"
+  else
+    #change this redirect to pass the correct session ID
+    redirect '/users/1/questions/new'
+  end
 end
 
 get '/users/:user_id/questions/:id' do
